994023f1525ef36cbfdfc979b810f0f5119ed334
KONFeature <quentin@nivelais.com>
2025-06-10 09:51:37 +0200

    ✨ Check onboardign status on app index

--- app/routes/app.onboarding.$step.tsx
+++ app/routes/app.onboarding.$step.tsx
@@ -2,29 +2,8 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { BlockStack, Card, Page } from "@shopify/polaris";
 import { Stepper } from "app/components/Stepper";
-import {
-    type FirstProductPublishedReturnType,
-    firstProductPublished,
-    shopInfo,
-} from "app/services.server/shop";
-import {
-    type GetMainThemeIdReturnType,
-    doesThemeHasFrakActivated,
-    doesThemeHasFrakButton,
-    doesThemeHasFrakWalletButton,
-    getMainThemeId,
-} from "app/services.server/theme";
-import {
-    type GetWebPixelReturnType,
-    getWebPixel,
-} from "app/services.server/webPixel";
-import {
-    type GetWebhooksSubscriptionsReturnType,
-    frakWebhookStatus,
-    getWebhooks,
-} from "app/services.server/webhook";
 import { authenticate } from "app/shopify.server";
-import type { AuthenticatedContext } from "app/types/context";
+import { stepDataFetchers } from "app/utils/onboarding";
 import { useTranslation } from "react-i18next";
 
 /**
@@ -36,66 +15,10 @@ function parseStepNumber(step?: string) {
     return Number(step.replace(/\D/g, "")); // Extract only numbers
 }
 
-type StepData = {
-    webPixel?: GetWebPixelReturnType;
-    webhooks?: GetWebhooksSubscriptionsReturnType["edges"];
-    isThemeHasFrakActivated?: boolean;
-    isThemeHasFrakButton?: boolean;
-    theme?: GetMainThemeIdReturnType;
-    firstProduct?: FirstProductPublishedReturnType;
-    themeWalletButton?: string | null;
-    frakWebhook?: {
-        setup: boolean;
-    };
-    productId?: string;
-};
-
-/**
- * Handlers for each step that will fetch the data needed
- */
-const stepHandlers = {
-    3: async (context: AuthenticatedContext): Promise<StepData> => {
-        try {
-            const webPixel = await getWebPixel(context);
-            return { webPixel };
-        } catch (error) {
-            console.error(error);
-            return {};
-        }
-    },
-
-    4: async (context: AuthenticatedContext): Promise<StepData> => {
-        const webhooks = await getWebhooks(context);
-        const shop = await shopInfo(context);
-        const frakWebhook = await frakWebhookStatus({
-            productId: shop.productId,
-        });
-        return { webhooks, frakWebhook, productId: shop.productId };
-    },
-
-    5: async (context: AuthenticatedContext): Promise<StepData> => {
-        const [isThemeHasFrakActivated, theme] = await Promise.all([
-            doesThemeHasFrakActivated(context),
-            getMainThemeId(context.admin.graphql),
-        ]);
-        return { isThemeHasFrakActivated, theme };
-    },
-
-    6: async (context: AuthenticatedContext): Promise<StepData> => {
-        const [isThemeHasFrakButton, firstProduct, themeWalletButton] =
-            await Promise.all([
-                doesThemeHasFrakButton(context),
-                firstProductPublished(context),
-                doesThemeHasFrakWalletButton(context),
-            ]);
-        return { isThemeHasFrakButton, firstProduct, themeWalletButton };
-    },
-};
-
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     const context = await authenticate.admin(request);
     const stepNumber = parseStepNumber(params.step);
-    const stepHandler = stepHandlers[stepNumber as keyof typeof stepHandlers];
+    const stepHandler = stepDataFetchers[stepNumber as keyof typeof stepDataFetchers];
     const stepData = stepHandler ? await stepHandler(context) : {};
 
     return {
